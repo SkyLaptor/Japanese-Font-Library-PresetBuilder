c7a757056e6452c53bfb900395ffddd7245ed913
SkyLaptor <gbreakmasterfrom@gmail.com>
2026-02-24 11:25:07 +0900

    Fixed
    パス名を修正

--- src/models/cache.py
+++ src/models/cache.py
@@ -11,10 +11,10 @@ from src.const import (
 
 class Cache:
     def __init__(self, cache_path: Path):
-        self.path = cache_path  # パスを保持する変数を分ける
+        self.cache_path = cache_path  # パスを保持する変数を分ける
         self.data = []  # 中身を data にする
 
-        if not self.path.exists():
+        if not self.cache_path.exists():
             print("キャッシュファイルが存在しないため、空ファイルを生成します。")
             self.save()
         else:
@@ -22,9 +22,9 @@ class Cache:
 
     def load(self):
         """YAMLファイルからキャッシュを読み込む"""
-        if self.path.exists():
+        if self.cache_path.exists():
             try:
-                with open(self.path, "r", encoding=ENCODE) as f:
+                with open(self.cache_path, "r", encoding=ENCODE) as f:
                     loaded_data = yaml.safe_load(f)
                     if isinstance(loaded_data, list):
                         self.data = loaded_data
@@ -35,8 +35,8 @@ class Cache:
         """現在のキャッシュをYAMLファイルに保存する"""
         try:
             # 親ディレクトリがなければ作成
-            self.path.parent.mkdir(parents=True, exist_ok=True)
-            with open(self.path, "w", encoding=ENCODE) as f:
+            self.cache_path.parent.mkdir(parents=True, exist_ok=True)
+            with open(self.cache_path, "w", encoding=ENCODE) as f:
                 # Dumper を指定して、アンカー/エイリアスを無効化する
                 class NoAliasDumper(yaml.SafeDumper):
                     def ignore_aliases(self, data):
